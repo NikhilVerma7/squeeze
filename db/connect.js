85331427c8fe1c041ea7a4f86743c5733fd19ae8
NIKHIL VERMA <nikhilverma007007@gmail.com>
2024-01-21 22:23:05 +0530

    after dircet accect to db

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 async function connectToMongoDB(url){
-    return mongoose.connect(url);
+    // return mongoose.connect(url);
+    mongoose.connect('mongodb://127.0.0.1:27017/shrinkIt'); 
+
 }
 
-// mongoose.connect('mongodb://127.0.0.1:27017/shrinkIt'); 
+
 
 module.exports = connectToMongoDB;
